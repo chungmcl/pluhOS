2a95ecf80032611e18413a9648c628678c0e2e21
Micheal Chung <chungmcl@apple.com>
2024-10-15 21:37:21 -0700

    dug a litlte into the DTB QEMU passes to us

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdint.h>
 
 int main() {
+  // TODO: parse the device tree with https://github.com/dgibson/dtc
+  // note that the device tree is in big endian while this CPU is lil endian
   uint8_t* device_tree_address;
   asm volatile (
       "mov %0, x1" // Get the device tree's address out of x1
